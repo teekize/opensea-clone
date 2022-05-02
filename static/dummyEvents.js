055af2c5e989f87cc251f90e6a50bb679fded654
teekize <elvisteeka1@gmail.com>
2022-05-02 15:02:35 -0700

    final-app

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: '0xLenny',
+    to: '0xLemein',
+    date: '4 months ago',
+  },
+  {
+    price: 17,
+    from: '0xLenny',
+    to: '0xLemein',
+    date: '4 months ago',
+  },
+  {
+    price: 17,
+    from: '0xLenny',
+    to: '0xLemein',
+    date: '4 months ago',
+  },
+  {
+    price: 17,
+    from: '0xLenny',
+    to: '0xLemein',
+    date: '4 months ago',
+  },
+]
